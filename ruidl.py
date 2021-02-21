a6327e29d22a48a08654f0b8c0b84a1689294708
Caleb Lawson <caleb.g.lawson@gmail.com>
2021-02-21 11:07:07 -0500

    support for reddit and imgur albums, imgur, gfycat

--- ruidl.py
+++ ruidl.py
@@ -3,6 +3,7 @@ This file contains the necessary components to download images from a subbreddit
 '''
 
 import os
+import re
 import json
 import hashlib
 
@@ -18,8 +19,15 @@ import requests
 import praw
 import typer
 import wordninja
+from bs4 import BeautifulSoup
 
 APP = typer.Typer()
+
+USER_AGENT_HEADER = {
+    'User-Agent': (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
+    )
+}
 
 
 def _make_config():
@@ -46,12 +54,25 @@ def _make_api(config):
     return api
 
 
+def _ninjify(url):
+    words = wordninja.split(url.split("/")[-1])
+
+    capitalized_words = ''
+    for idx, word in enumerate(words):
+        if idx < 3:
+            capitalized_words = f'{capitalized_words}{word[0].upper()}{word[1:]}'
+        else:
+            capitalized_words = f'{capitalized_words}{word}'
+
+    return capitalized_words
+
+
 class Ruidl:
     '''
     Reddit media downloader.
     '''
 
-    def __init__(self, name, wordninja_only):
+    def __init__(self, name):
         self._name = name
 
         self._config = _make_config()
@@ -65,8 +86,9 @@ class Ruidl:
             '.jpg',
             '.png',
             '.gif',
-            '.mp4'
-        ] if not wordninja_only else []
+            '.mp4',
+            '.webm'
+        ]
         existing_files = []
         for file_type in self._filetypes:
             existing_files.extend(glob(f'{self._base_path}/*{file_type}'))
@@ -80,57 +102,31 @@ class Ruidl:
             '.\\wordninja_words.txt.gz'
         )
 
-    def _process_submission(self, submission):
+    def _download_file(self, file_url, submission):
+        file_name = f'{self._base_path}/{file_url.split("/")[-1]}'
+
+        if file_name in self._filenames:
+            return
+
+        request = requests.get(file_url, headers=USER_AGENT_HEADER)
+        file_hash = hashlib.md5(request.content).hexdigest()
+        if file_hash in self._checksums:
+            return
+
+        self._checksums.add(file_hash)
+        self._filenames.add(file_name.split("/")[-1].split("?")[0])
+        new_file_name = f'{self._base_path}/{file_hash}_{file_name.split("/")[-1].split("?")[0]}'
+
+        if sys.getsizeof(request.content) < self._config.get('file_size_threshold', 10000):
+            return
+
+        with open(new_file_name, 'wb') as new:
+            new.write(request.content)
+
+        if 'mp4' in new_file_name or 'gif' in new_file_name or 'webm' in new_file_name:
+            return
+
         try:
-            if (
-                    self._config.get('wordninja_trigger') and
-                    self._config.get('wordninja_trigger') in submission.url
-            ):
-                words = wordninja.split(submission.url.split("/")[-1])
-
-                capitalized_words = ''
-                for idx, word in enumerate(words):
-                    if idx < 3:
-                        capitalized_words = f'{capitalized_words}{word[0].upper()}{word[1:]}'
-                    else:
-                        capitalized_words = f'{capitalized_words}{word}'
-
-                submission.url = (
-                    f'{self._config.get("wordninja_download_url","")}'
-                    f'{capitalized_words}.mp4'
-                )
-            elif any([ext in submission.url for ext in self._filetypes]):
-                pass
-            else:
-                if self._config.get('verbose'):
-                    typer.echo(
-                        f'No matches triggered for this URL: {submission.url}'
-                    )
-                return
-
-            file_name = f'{self._base_path}/{submission.url.split("/")[-1]}'
-
-            if file_name in self._filenames:
-                return
-
-            request = requests.get(submission.url)
-            file_hash = hashlib.md5(request.content).hexdigest()
-            if file_hash in self._checksums:
-                return
-
-            self._checksums.add(file_hash)
-            self._filenames.add(file_name.split("/")[-1])
-            new_file_name = f'{self._base_path}/{file_hash}_{file_name.split("/")[-1]}'
-
-            if sys.getsizeof(request.content) < self._config.get('file_size_threshold', 10000):
-                return
-
-            with open(new_file_name, 'wb') as new:
-                new.write(request.content)
-
-            if 'mp4' in new_file_name or 'gif' in new_file_name:
-                return
-
             with open(new_file_name, 'rb') as new:
                 image = exif.Image(new.read())
                 image.artist = str(submission.author)
@@ -138,6 +134,76 @@ class Ruidl:
 
             with open(new_file_name, 'wb') as new:
                 new.write(image.get_file())
+        except Exception as exception:  # pylint: disable=broad-except
+            if self._config.get('verbose'):
+                typer.echo(
+                    f'Error writing Exif data: {new_file_name} {exception}'
+                )
+
+    def _get_file_urls(self, submission):
+        file_urls = []
+        if any([ext in submission.url for ext in self._filetypes]):
+            file_urls = [submission.url]
+        elif 'reddit.com/gallery' in submission.url:
+            request = requests.get(
+                submission.url,
+                headers=USER_AGENT_HEADER
+            )
+            soup = BeautifulSoup(request.content, features='html.parser')
+            file_urls = [
+                elem.get('href')
+                for elem in
+                soup.find_all(
+                    'a',
+                    href=re.compile('preview.redd.it'),
+                    attrs={'target': '_blank'}
+                )
+            ]
+        elif 'imgur.com/a/' in submission.url:
+            # Imgur gallery, multiple images.
+            gallery = submission.url.split("/")[-1]
+            request = requests.get(
+                f'https://imgur.com/ajaxalbums/getimages/{gallery}',
+                headers=USER_AGENT_HEADER
+            )
+            response = json.loads(request.content)
+            file_urls = [
+                f'https://i.imgur.com/{image["hash"]}{image["ext"]}'
+                for image in
+                response['data']['images']
+            ]
+        elif 'imgur.com/' in submission.url:
+            # Single imgur image.
+            image = submission.url.split("/")[-1]
+            file_urls = [f'https://i.imgur.com/{image}.jpg']
+        elif (
+                self._config.get('wordninja_trigger') and
+                self._config.get('wordninja_trigger') in submission.url
+        ):
+            file_urls = [
+                (
+                    f'{self._config.get("wordninja_download_url","")}'
+                    f'{_ninjify(submission.url)}.mp4'
+                )
+            ]
+        elif 'gfycat.com/' in submission.url:
+            file_urls = [
+                f'https://giant.gfycat.com/{_ninjify(submission.url)}.webm'
+            ]
+        else:
+            if self._config.get('verbose'):
+                typer.echo(
+                    f'No match triggered for this URL: {submission.url}'
+                )
+
+        return file_urls
+
+    def _process_submission(self, submission):
+        try:
+            file_urls = self._get_file_urls(submission)
+
+            for file_url in file_urls:
+                self._download_file(file_url, submission)
 
         except Exception as exception:  # pylint: disable=broad-except
             # Needed so that any exceptions in threads are loud and clear.
@@ -194,26 +260,24 @@ class Ruidl:
 @APP.command()
 def redditor(
         name: str,
-        limit: int = typer.Option(None),
-        wordninja_only: bool = typer.Option(False)
+        limit: int = typer.Option(None)
 ):
     '''
     Download from the specified user.
     '''
-    Ruidl(name, wordninja_only).redditor(limit)
+    Ruidl(name).redditor(limit)
 
 
 @APP.command()
 def subreddit(
         name: str,
         search: str = typer.Option(None),
-        limit: int = typer.Option(None),
-        wordninja_only: bool = typer.Option(False)
+        limit: int = typer.Option(None)
 ):
     '''
     Download from the specified subreddit.
     '''
-    Ruidl(name, wordninja_only).subreddit(search, limit)
+    Ruidl(name).subreddit(search, limit)
 
 
 if __name__ == '__main__':
